1ab2366e7313a9f0f0cee3a6a19a29e4b2588569
oytundemirbilek <oytun1996@gmail.com>
2020-01-01 17:36:06 +0000

    IP address table is added

--- server.py
+++ server.py
@@ -1,11 +1,24 @@
 import os
-
+import pandas as pd
 from bottle import Bottle, run, route, template, static_file, request, get
 
 
 def home_page():
     client_ip = request.environ.get('REMOTE_ADDR')
-    return template('./static/home')
+    ip_data = pd.read_csv('ip_address.csv')
+    
+    if ip_data.empty:
+        ip_data = pd.DataFrame([[client_ip, 1]], columns=['IP ADDRESS','COUNT'])
+    else:
+        filtering = ip_data['IP ADDRESS'] == client_ip
+        if ip_data[filtering].empty:
+            add = pd.DataFrame([[client_ip, 1]], columns=['IP ADDRESS','COUNT'])
+            ip_data = ip_data.append(add, ignore_index=True)
+        else:
+            ip_data.loc[filtering,'COUNT'] += 1
+
+    ip_data.to_csv('ip_address.csv', index = False)
+    return ip_data.to_html()
 
 def register():
     if request.method == 'POST':
@@ -26,9 +39,19 @@ def project_page():
 def contact_page():
     return template('./static/contact')
 
-@route('/server/<filename>')
-def server_style(filename):
-    return static_file(filename, root='./style')
+@route('/static/<path:path>')
+def server_static(path):
+    print(os.getcwd() + '/static' + path)
+    return static_file(path, root=os.getcwd() + '/static')
+
+def get_style():
+    return static_file("index.css", root='./static/style')
+def get_banner():
+    return static_file("ai.png", root='./static/style')
+def get_profile():
+    return static_file("pp.png", root='./static/style')
+def get_uni():
+    return static_file("itulogo.jpg", root='./static/style')
 
 
 def create_app():
@@ -38,9 +61,12 @@ def create_app():
     app.route("/overview", "GET", about)
     app.route("/projects", "GET", project_page)
     app.route("/contact", "GET", contact_page)
+    app.route("/server/index.css", "GET", get_style)
+    app.route("/server/ai.png", "GET", get_banner)
+    app.route("/server/pp.png", "GET", get_profile)
+    app.route("/server/itulogo.jpg", "GET", get_uni)
 
     return app
-
 
 
 if __name__ == '__main__':
